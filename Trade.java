1ea19cfd189b71baa628e6d2cb4da221ebf8cc27
이은채 <58711995+Lee-Eunchae@users.noreply.github.com>
2020-02-12 00:13:34 +0900

    Add files via upload

---
+++ Trade.java
@@ -0,0 +1,23 @@
+public class Trade extends Main
+{
+
+	//private static gagaeName;
+
+	/*
+	public void managerToday() // 거래 총 실적을 나타낼 메소드
+	{
+		System.out.println("------------------------");
+		System.out.printf("\t[이용 횟수 : %d]\n", nujeok);
+		System.out.println();
+		//System.out.printf(" 가게 : %s\n", str); 입력받을 가게이름
+		//System.out.printf(" 메뉴 : %s\n", strr); 입력받을 메뉴이름
+		System.out.printf(" 금액 : %d\n", ghan.get(str).menu.keySet());
+		System.out.println();
+		System.out.println("------------------------");
+		System.out.println();
+		System.out.println();
+
+	}
+	*/
+	
+}
